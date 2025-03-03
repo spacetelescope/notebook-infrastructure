18fdea33e344d168f7d342299fa5ba2c2f0c00b4
Thomas Dutkiewicz <106269091+ttdu@users.noreply.github.com>
2025-03-02 20:10:41 -0500

    add more comments, clean up code

--- tools/requirements-builder/arrays.py
+++ tools/requirements-builder/arrays.py
@@ -1,25 +1,30 @@
 import numpy as np
 
-#a = [[0,2,1],[2,0,3],[1,3,0]]
-
 def setup_distances_matrix(a):
-    # you actually need a square root to calculate distances,
-    # but we don't care about the absolute distance, only the relative.
-    # thus, let's skip the extra computational step
+    """ create a matrix with the distance from each notebook to all others.
+    "distance" is the number of packages different from one notebook to the next
+    you actually need a square root to calculate distances,
+    but we don't care about the absolute distance, only the relative.
+    thus, let's skip the extra computational step"""
     return np.sum(np.square(a-a[:,None]), axis=-1)
 
 def drop_notebook_dist(input_matrix, idx):
+    """Drop the row and column corresponding to a particular notebook
+    (Should be used with the most distant notebook, on distance matrix)"""
     deleted_row = np.delete(input_matrix, idx, axis=0)
     deleted_column = np.delete(deleted_row, idx, axis=1)
     return deleted_column
 
 def drop_notebook_reqs(input_matrix, idx):
+    """ Drop the row corresponding to a particular notebook and
+    check for unused packages. Should be used on requirements matrix
+
+    """
     deleted_row = np.delete(input_matrix, idx, axis=0)
     column_sum = np.sum(input_matrix, axis=0)
     dropped_packages = column_sum == 0
     return deleted_row, dropped_packages
 
-#print(drop_notebook(a, np.array([[1],[2]])))
 
 def nb_packages_to_row(pack_dict, nb_packages):
     """ Each package has a numerical index (in pack_dict).
@@ -56,8 +61,3 @@ def get_packages_only(nb_dict):
     # need to flatten out this list of lists
     nbd = [x for xs in nb_dict.values() for x in xs]
     return set(nbd)
-
-# pd = {"numpy":0, "astropy":1, "fake_package":2}
-# nbp = ["fake_package", "numpy"]
-
-# print(nb_packages_to_row(pd, nbp))
--- tools/requirements-builder/reqs-builder.py
+++ tools/requirements-builder/reqs-builder.py
@@ -1,11 +1,3 @@
-## NOTES
-# I think this is pretty close to maximum efficiency, but
-# it would converge faster if pk_counter were recomputed after each step because
-# if recomputed, it could skip packages that were only requested by one notebook
-# 
-# 1. there would be fewer requirements (in the case where one notebook is the only one requesting multiple packages)
-# 2. if a notebook fails and is deleted, we no longer need to consider its packages
-
 import subprocess
 from collections import Counter
 import re
@@ -20,17 +12,10 @@ grep_out, err = p1.communicate()
 
 # convert bytes to string
 grep_out = str(grep_out)
-#print(grep_out)
-
-# with open("testing-reqs.txt", "wb") as outfile:
-#     outfile.write(grep_out)
-
 
 # get all of the packages used, with the corresponding notebooks
 # a-zA-Z0-9 = normal package names, \+:\/\. = "include + / ." for github urls
 packages = re.findall(r"notebooks/(.*?):([a-zA-Z0-9\+:\/\.]+)", grep_out)
-#print(packages)
-print(packages)
 
 # get the list of requirements per notebook
 nb_reqs_dict = a.parse_mega_list(packages)
@@ -47,7 +32,6 @@ nb_matrix = np.array([a.nb_packages_to_row(numbered_packages, nb_req) for nb_req
 # create the distances matrix, which we'll need for later
 dist_matrix = a.setup_distances_matrix(nb_matrix)
 
-
 # we need to iterate: 
 # 1 - calculate distances
 # 2 - drop largest distance notebook(s) row/column from distance matrix
@@ -57,33 +41,31 @@ dist_matrix = a.setup_distances_matrix(nb_matrix)
 #     number of rows = passing notebooks
 #     number of non-zero columns = number of packages
 
+# arrays for convenient use later
 lap = np.array(list(all_packages))
 drop = np.array([])
 
+# while we have more than two notebooks, continue
 while len(dist_matrix>2):
-    # calculate distances
+    # calculate distances -- "furthest" notebooks should be dropped
     s = np.sum(dist_matrix, axis=0)
     drop_idx = np.argwhere(max(s)==s)
 
-    # drop values
+    # drop value(s) calculated above
     dist_matrix = a.drop_notebook_dist(dist_matrix, drop_idx)
     nb_matrix, dropped_packages = a.drop_notebook_reqs(nb_matrix, drop_idx)
 
+    # current packages we are removing/dropping
     current_drop = lap[dropped_packages]
     
+    # catch the case on the first run so we don't run setdiff1d
     if current_drop.size >= 1 and drop.size >= 1:
         new_drops = np.setdiff1d(current_drop, drop)
         print(len(nb_matrix)+len(drop_idx), new_drops)
     elif current_drop.size >= 1 and drop.size < 1:
         print(len(nb_matrix)+len(drop_idx), current_drop)
     
+    # add the latest dropped packages to the list
     drop = np.concatenate((drop, current_drop))
  
-
-    #print(drop)
-
-
-# we need a notebook translation matrix: 
-# when I drop a nb, how do I tell its requirements?
-# oh, wait. it's the row of the notebook matrix 
 
